7c2ae386cde4112fe4cf03a3c9f49056fd620206
G <gordon.liang.xu@gmail.com>
2024-06-20 00:25:22 -0400

    router and markdowns

--- frontend/src/ProjectView.tsx
+++ frontend/src/ProjectView.tsx
@@ -24,15 +24,16 @@ export const ProjectView = ({projectJson}: {projectJson:any}) => {
   return (
     <div id='md' style={{minHeight: '100vh', display: 'flex', flexDirection: 'column'}}>
       {!mdString && <p className='loading'>Loading...</p>}
-      {(mdString && projectObj.demovid) && <iframe
-        width="853"
-        height="480"
-        src={projectObj.demovid}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        style={{border: '1px solid #242424', borderRadius: 8, overflow: 'hidden', alignSelf: 'center'}}
-      />}
-      {mdString && <div style={{maxWidth: '853px', paddingBottom: '64px'}}>
+      {(mdString && projectObj.demovid) && 
+      <div className="videoWrapper">
+        <iframe
+          src={projectObj.demovid}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </div>
+      }
+      {mdString && <div style={{maxWidth: '853px', padding: '16px 16px 64px 16px'}}>
         {projectObj.link !== '' && <p>Check it out here: <a href={projectObj.link}>{projectObj.link}</a></p>}
         <Markdown>{mdString}</Markdown>
       </div>}
